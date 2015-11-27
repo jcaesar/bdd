a5bc0327bb52f32045bdb94f24bf2dc1349b8fec
ammbauer <max.haslbeck@gmx.de>
2015-11-27 11:14:18 +0100

    New datatype for name

--- thy/BDDMax.thy
+++ thy/BDDMax.thy
@@ -3,33 +3,34 @@ imports Main
 begin
 
 
+datatype 'a AROBDD_uname = FalseSinkName | TrueSinkName | UniqueNodeName 'a
 
-datatype 'a AROBDD = FalseSink | TrueSink | UniqueNode 'a "'a AROBDD" "'a AROBDD"
+datatype 'a AROBDD = FalseSink | TrueSink | UniqueNode "'a AROBDD_uname" "'a AROBDD" "'a AROBDD"
 
 context linorder
 begin
 
-fun AROBDD_name :: "'a AROBDD \<Rightarrow> 'a option" where
-  "AROBDD_name (UniqueNode v _ _) = Some v" |
-  "AROBDD_name FalseSink = None" |
-  "AROBDD_name TrueSink = None"
+fun AROBDD_name :: "'a AROBDD \<Rightarrow> 'a AROBDD_uname" where
+  "AROBDD_name (UniqueNode v _ _) = v" |
+  "AROBDD_name FalseSink = FalseSinkName" |
+  "AROBDD_name TrueSink = TrueSinkName"
 
-fun AROBDD_nodes :: "'a AROBDD \<Rightarrow> 'a set" where
+fun AROBDD_nodes :: "'a AROBDD \<Rightarrow> 'a AROBDD_uname set" where
   "AROBDD_nodes (UniqueNode v l r) = {v} \<union> (AROBDD_nodes l) \<union> (AROBDD_nodes r)" |
   "AROBDD_nodes FalseSink = {}" |
   "AROBDD_nodes TrueSink = {}"
 
-fun AROBDD_ordered :: "'a AROBDD \<Rightarrow> bool" where
-  "AROBDD_ordered (UniqueNode v l r) = ((\<forall>ln \<in> AROBDD_nodes l. ln < v) \<and> 
-                                         (\<forall>rn \<in> AROBDD_nodes r. rn < v))" |
-  "AROBDD_ordered FalseSink = True" |
-  "AROBDD_ordered TrueSink = True"
+(* Maybe use option *)
+fun AROBDD_uname_set :: "'a AROBDD_uname \<Rightarrow> 'a set" where
+  "AROBDD_uname_set (UniqueNodeName v) = {v}" |
+  "AROBDD_uname_set FalseSinkName = {}" |
+  "AROBDD_uname_set TrueSinkName = {}"
 
-(* Keine Ahnung, wie ich linorder auf 'b bekomme. Man könnte auch 'a nochmal verwenden *)
-(* Und dann noch ('b \<Rightarrow> bool) für das richtige variablen mapping *)
-
-definition nodename_to_boolvar_map_restrict :: "('a \<Rightarrow> 'b) \<Rightarrow> bool" where
-  "dings f \<equiv> a < b \<Longrightarrow> f a < f b"
+fun AROBDD_nodes_names :: "'a AROBDD \<Rightarrow> 'a set" where
+  "AROBDD_nodes_names (UniqueNode v l r) = AROBDD_uname_set v \<union> (AROBDD_nodes_names l) 
+                                           \<union> (AROBDD_nodes_names r)" |
+  "AROBDD_nodes_names FalseSink = {}" |
+  "AROBDD_nodes_names TrueSink = {}"
 
 end
 end
