5ad1ccebec6ce81c4dd0ad36fc4e8181c6e242db
ammbauer <max.haslbeck@gmx.de>
2015-11-27 13:11:56 +0100

    With hopefully correct function definitons

--- Boolean_Expression_Checkers/BDT.thy
+++ Boolean_Expression_Checkers/BDT.thy
@@ -13,22 +13,30 @@ fun ifex_variable_set :: "'a ifex \<Rightarrow> 'a set" where
   "ifex_variable_set Falseif = {}"
 
 fun ordner :: "('a::linorder) ifex \<Rightarrow> bool" where
-  "ordner (IF v t e) = ((\<forall>tv \<in> ifex_variable_set t. v < tv) \<and> (\<forall>ev \<in> ifex_variable_set t. v < ev))" |
+  "ordner (IF v t e) = ((\<forall>tv \<in> ifex_variable_set t. v < tv) \<and> (\<forall>ev \<in> ifex_variable_set t. v < ev)
+                       \<and> ordner t \<and> ordner e)" |
   "ordner Trueif = True" |
   "ordner Falseif = True"
 
 
 definition ifex_bf2_rel where
-  "ifex_bf2_rel = {(a,b) | a b. \<forall>ass. a ass \<longleftrightarrow> val_ifex b ass \<and> ordner b}"
+  "ifex_bf2_rel = {(a,b) | a b. (\<forall>ass. a ass \<longleftrightarrow> val_ifex b ass)  \<and> ordner b}"
 
 definition select where "select a = undefined"
 
 fun restrict where
-  "restrict (IF v t e) var val = (if v = var then restrict (if val then t else e) var val else (IF v (restrict t var val) (restrict e var val)))" |
+  "restrict (IF v t e) var val = (let rt = restrict t var val; re = restrict e var val in
+                   (if v = var then (if val then rt else re) else (IF v rt re)))" |
   "restrict i _ _ = i"
   
+value "ordner (IF 1 Falseif (IF (0::nat) (IF 0 Falseif Falseif) Falseif))"
+value "restrict (IF (1::nat) Falseif (IF 0 (IF 0 Falseif Falseif) Falseif)) 1 False"
+(* definition "bf2_restrict (i::nat) (val::bool) (func::boolfunc2) \<equiv> (\<lambda>v. func (v(i:=val)))" *)
+value "ordner (IF (0::nat) (IF 0 Falseif Falseif) Falseif)"
+
 lemma "(a, b) \<in> ifex_bf2_rel \<Longrightarrow> restrict b var val = b' \<Longrightarrow> bf2_restrict var val a = a' \<Longrightarrow> (a', b') \<in> ifex_bf2_rel"
-oops (* TODO *)
+  unfolding ifex_bf2_rel_def 
+oops
 
 function dings :: "'a ifex \<Rightarrow> 'a ifex \<Rightarrow> 'a ifex \<Rightarrow> 'a ifex" where
   "dings Trueif t e = t" |
