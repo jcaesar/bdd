67e6711aca7170f71b308612dba7eb12708dc24f
Caesar <gitter@liftm.de>
2016-01-17 16:08:08 +0100

    Oh. There were some instances of default missing.

--- thy/Conc_Impl.thy
+++ thy/Conc_Impl.thy
@@ -2,51 +2,33 @@ theory Conc_Impl
 imports PointerMapImpl TestImpl1
 begin
 
-datatype nat_trip = NatTrip (tfst: nat) (tsnd: nat) (ttrd: nat)
-(*type_synonym nat_trip = "(nat \<times> nat \<times> nat)" Instance won't eat this.*)
-term hashcode
-instantiation nat_trip :: default
+instantiation prod :: (default, default) default
 begin
-	definition "default_nat_trip \<equiv> NatTrip 0 0 0"
+	definition "default_prod :: ('a \<times> 'b) \<equiv> (default, default)" 
+	instance by(intro_classes)
+end
+(* can be found in "~~/src/HOL/Proofs/Extraction/Greatest_Common_Divisor" or "~~/src/HOL/Proofs/Lambda/WeakNorm" *)
+instantiation nat :: default
+begin
+	definition "default_nat \<equiv> 0 :: nat"
 	instance ..
 end
-instantiation nat_trip :: hashable
-begin
-  definition "hashcode_nat_trip x \<equiv> (hashcode (tfst x) * 33 + hashcode (tsnd x)) * 33 + hashcode (tsnd x)"
-  definition "def_hashmap_size = (\<lambda>_ :: nat_trip itself. def_hashmap_size TYPE(nat) * 2)"
-  instance using def_hashmap_size[where ?'a=nat] def_hashmap_size[where ?'a=nat]
-    by(intro_classes)(simp_all add: def_hashmap_size_nat_trip_def)
-end
-instantiation nat_trip :: heap
-begin
-	instance proof(intro_classes)
-		have "\<exists>(f::(nat \<times> nat) \<Rightarrow> nat). inj f" by (simp add: ex_inj)
-		then guess f ..
-		note injf = this
-		hence "inj (\<lambda>nt. f (f (tfst nt, tsnd nt), ttrd nt))"
-			apply -
-			apply(rule injI)
-			apply(simp add: inj_eq nat_trip.expand)
-		done
-		thus "\<exists>(g::nat_trip \<Rightarrow> nat). inj g" by auto
-	qed
-end
-
 
 definition "tci bdd \<equiv> return (1,bdd)"
 definition "fci bdd \<equiv> return (0,bdd)"
-definition "ifci v t e bdd \<equiv> do {
-	(p,u) \<leftarrow> pointermap_getmki (NatTrip v t e) bdd;
+definition "ifci v t e bdd \<equiv> (if t = e then return (t, bdd) else do {
+	(p,u) \<leftarrow> pointermap_getmki (v, t, e) bdd;
 	return (Suc (Suc p), u)
-}"
+})"
 
 lemma [sep_heap_rules]: "<is_pointermap_impl bdd bddi> tci bddi <\<lambda>r. is_pointermap_impl bdd (snd r) * \<up>(fst r = fst (tmi bdd))>"
 by(sep_auto simp: tci_def)
 lemma [sep_heap_rules]: "<is_pointermap_impl bdd bddi> fci bddi <\<lambda>r. is_pointermap_impl bdd (snd r) * \<up>(fst r = fst (fmi bdd))>"
 by(sep_auto simp: fci_def)
-lemma [sep_heap_rules]: "<is_pointermap_impl bdd bddi> ifci v t e bddi
-	<\<lambda>r. let (p,u) = ifmi v t e bdd in is_pointermap_impl bdd u * \<up>(fst r = p)>"
-apply(sep_auto simp: ifci_def)
+lemma [sep_heap_rules]: "(p, u) = ifmi v t e bdd \<Longrightarrow>
+	<is_pointermap_impl bdd bddi> ifci v t e bddi
+	<\<lambda>(pi,ui). is_pointermap_impl u ui * \<up>(pi = p)>\<^sub>t"
+by(sep_auto simp: ifci_def apfst_def map_prod_def split: prod.splits if_splits)
 
 
 end
