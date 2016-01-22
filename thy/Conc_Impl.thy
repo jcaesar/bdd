aa0760ed26345f8dc58e5bd534e700d0b82040cd
Caesar <gitter@liftm.de>
2016-01-22 22:56:27 +0100

    Concrete restrict_top

--- thy/Conc_Impl.thy
+++ thy/Conc_Impl.thy
@@ -41,5 +41,52 @@ lemma [sep_heap_rules]: "
 	<\<lambda>r. is_pointermap_impl bdd bddi * \<up>(r = destrmi n bdd)>"
 by(cases "(n, bdd)" rule: destrmi.cases) (sep_auto simp: destrci_def)+
 
+definition "restrict_topci p var val bdd = do {
+	d \<leftarrow> destrci p bdd;
+	return (case d of IFD v t e \<Rightarrow> (if v = var then (if val then t else e) else p) | _ \<Rightarrow> p)
+}"
+lemma [sep_heap_rules]: "
+	p < 2 \<or> pointermap_p_valid (p - 2) bdd \<Longrightarrow>
+	<is_pointermap_impl bdd bddi> restrict_topci p var val bddi
+	<\<lambda>r. is_pointermap_impl bdd bddi * \<up>(r = brofix.restrict_top_impl p var val bdd)>"
+by(sep_auto simp: restrict_topci_def)
+
+fun lowest_topsci where
+"lowest_topsci [] s = return None" |
+"lowest_topsci (e#es) s = 
+	(if e < 2 then lowest_topsci es s else do {
+		(c,_,_) \<leftarrow> pm_pthi s e;
+		rec \<leftarrow> lowest_topsci es s;
+		return (case rec of Some d \<Rightarrow> Some (min c d) | None \<Rightarrow> Some c)
+	})"
+term "(list_all (pointermap_p_valid\<inverse>\<inverse> bdd), bdd)" 
+lemma [sep_heap_rules]: "
+	list_all (\<lambda>e. e < 2 \<or> pointermap_p_valid (e - 2) bdd) es \<Longrightarrow>
+	<is_pointermap_impl bdd bddi> lowest_topsci es bddi
+	<\<lambda>r. is_pointermap_impl bdd bddi * \<up>(r = brofix.lowest_tops_impl es bdd)>"
+apply(induction es)
+apply(sep_auto simp:)
+apply(sep_auto simp: split: IFEXD.splits) 
+oops (* I doubt that sep_auto does induction. I'll check later, more pressing matters to attend. *)
+
+(* partial_function(option) ite_impl_opt where
+"ite_impl_opt i t e s =
+  (case DESTRimpl i s of TD \<Rightarrow> Some (t,s) | FD \<Rightarrow> Some (e,s) | _ \<Rightarrow>
+  (if DESTRimpl t s = TD \<and> DESTRimpl e s = FD then Some (i,s) else
+  (if e = t then Some (t,s) else
+	(case lowest_tops_impl [i, t, e] s of
+		Some a \<Rightarrow> (let
+			ti = restrict_top_impl i a True s;
+			tt = restrict_top_impl t a True s;
+			te = restrict_top_impl e a True s;
+			fi = restrict_top_impl i a False s;
+			ft = restrict_top_impl t a False s;
+			fe = restrict_top_impl e a False s
+			in do {
+			(tb,s) \<leftarrow> ite_impl_opt ti tt te s;
+			(fb,s) \<leftarrow> ite_impl_opt fi ft fe s;
+            Some (IFimpl a tb fb s)}) |
+        None \<Rightarrow> Some (case DESTRimpl i s of TD \<Rightarrow> (t, s) | FD \<Rightarrow> (e, s))))))"*)
+
 
 end
